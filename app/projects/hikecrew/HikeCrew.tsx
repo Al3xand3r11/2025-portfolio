fbb0adb1e18f45a94863f86e43595f1769efa35a
Al3xand3r11 <alexander.nance11@gmail.com>
2025-03-27 17:57:55 -0700

    start of prjects

---
+++ app/projects/hikecrew/HikeCrew.tsx
@@ -0,0 +1,5 @@
+export default function HikeCrew() {
+    return (
+        <p>Hike</p>
+    )
+}
---
+++ app/projects/sundayrunday/sundayrunday.tsx
@@ -0,0 +1,5 @@
+export default function SundayRunday () {
+    return (
+        <p>SundayRunday</p>
+    )
+}
