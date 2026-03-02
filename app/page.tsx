66cba0fe91b06391651b6fd4fba7c9405a935d30
Al3xand3r11 <alexander.nance11@gmail.com>
2026-03-01 21:43:08 -0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero";
+import About from "./components/About";
 import Work from "./components/Work";
 import StatsBar from "./components/StatsBar";
 import Community from "./components/Community";
@@ -9,6 +10,7 @@ export default function Home() {
     <main>
       <Hero />
       <StatsBar />
+      <About />
       <Work />
       <Community />
       <Contact />
